81ea7f2dee33549f673b83ce0c61170261160017
kevindonovanagetipa <kevin.agetipa@gmail.com>
2026-06-19 20:29:28 +0300

    feat: change api url

--- constants/api.ts
+++ constants/api.ts
@@ -1,1 +1,5 @@
-export const API_BASE_URL = 'http://192.168.1.100:5000';
+const USE_EMULATOR = false;
+
+export const API_BASE_URL = USE_EMULATOR
+  ? 'http://192.168.88.74:5000'
+  : 'http://192.168.1.100:5000';
